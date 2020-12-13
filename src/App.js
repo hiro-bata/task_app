8b8e71ab86f153c34c7b6fa6277102d4d868a596
hiro-bata <kawa.060313@gmail.com>
2020-12-14 04:32:00 +0900

    install_library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>test</h1>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Hello React!!</h1>
+      </div>
+    );
+  };
+
 }
 
 export default App;
